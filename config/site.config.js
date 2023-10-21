6371d14028c5dce91e13b538e4eb97a2d27b0e0a
Sizukaze <53135795+Sizukaze@users.noreply.github.com>
2023-10-21 18:37:25 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -35,7 +35,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by <a href="https://microsoft.sharepoint.com" target="_blank" rel="noopener noreferrer">Microsoft-Sharepoint</a>. Made with 🌍 by yuuki.',
+    'Powered by <a href="https://www.swordart-online.net/mothers" target="_blank" rel="noopener noreferrer">Mother's Rosario</a>. Made with 🌍 by yuuki.',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
@@ -49,8 +49,8 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'microsoft',
-      link: 'https://www.microsoft365.com',
+      name: 'swords',
+      link: 'https://www.swordart-online.net/',
     },
   ],
 
