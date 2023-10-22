2f90fbb23a264b0c98396376d987e0506915f891
Sizukaze <53135795+Sizukaze@users.noreply.github.com>
2023-10-22 08:54:03 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -49,7 +49,7 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'swords',
+      name: 'star-of-david',
       link: 'https://www.swordart-online.net',
     },
   ],
