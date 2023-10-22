481a4337a32bb420d7b2b963fdb276866e141ed6
Sizukaze <53135795+Sizukaze@users.noreply.github.com>
2023-10-22 10:18:11 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -21,7 +21,7 @@ module.exports = {
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
-  maxItems: 100,
+  maxItems: 180,
 
   // [OPTIONAL] We use Google Fonts natively for font customisations.
   // You can check and generate the required links and names at https://fonts.google.com.
@@ -49,8 +49,8 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'star-of-david',
-      link: 'https://www.swordart-online.net',
+      name: 'cloudflare',
+      link: 'https://developers.cloudflare.com',
     },
   ],
 
